f28147ead890afb55f6a9f7cfe0c93fa0aea6ae2
tamiso132 <59596626+tamiso132@users.noreply.github.com>
2023-04-18 00:22:10 +0200

    Update ObjectStatus.java

--- src/ObjectStatus.java
+++ src/ObjectStatus.java
@@ -3,6 +3,7 @@ public class ObjectStatus {
     public boolean isDeath;
     public boolean isFloat;
     public boolean isMelt;
+    public boolean isHot;
     public final ObjectType objectType;
     public ObjectStatus(ObjectType objectType, boolean isDeath, boolean isPushable, boolean isFloat){
         this.objectType = objectType;
